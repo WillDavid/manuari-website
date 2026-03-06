6da675ec7fda3275054ede21fe9690ccfbf5a8c1
William David <will@MacBook-Air-de-William.local>
2026-03-06 16:45:30 -0400

    remove prerender

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    prerender({
-      routes: [
-        '/',
-        '/produtos',
-        '/produtos/canecas',
-        '/produtos/xicaras',
-        '/produtos/azulejos',
-        '/para-empresas',
-        '/sobre'
-      ]
-    })
-  ]
+  plugins: [vue()],
 })
